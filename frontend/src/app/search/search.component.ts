240dc4979c54dbb54800a5c1decd716290eeb878
gajjela <kathyayanigajjela@gmail.com>
2025-04-03 18:31:12 +0530

    fourth_commit

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,inject ,OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { WeatherforecastService } from '../weatherforecast.service';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  standalone:true,
+  imports: [FormsModule,CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+ //cartItems:any[]=[];
+ inputs:string='';
+ storeValue(){
 
+ }
+ weatherForecasts:any[]=[];
+ weatherForecastService=inject(WeatherforecastService);
+   constructor(private dataService:DataService){
+     this.weatherForecastService.get().subscribe(weatherForecasts=>{
+      this.weatherForecasts=weatherForecasts;
+     })
+   }
+   /*ngOnInit(){
+    this.dataService.currentCart.subscribe(items=>{
+      this.cartItems=items;
+      
+      
+    });*/
+    senddata(weatherForecast:any){
+      this.dataService.addItemToCart(weatherForecast)
+    }
+  
 }
